bcd4ab90e1cd0ef2de86490c9933cda165d32be3
kc <choudhary.kus@northeastern.edu>
2022-11-03 09:16:34 -0400

    commit

---
+++ _helpers/db.js
@@ -0,0 +1,34 @@
+const config = require('db-config');
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
+
+module.exports = db = {};
+
+initialize();
+
+async function initialize() {
+    const { host, user, password, database, port } = config.database;
+    const connection = await mysql.createConnection({ host, port, user, password });
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+    const sequelize = new Sequelize(config.database.database, config.database.user, config.database.password, {
+        host: config.database.host,
+        dialect: "mysql",
+        operatorsAliases: false,
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
+        }
+      });
+
+
+    db.User = require('../users/user.model')(sequelize);
+    db.Document = require('../document/document-model')(sequelize);
+
+    db.Document.belongsTo(db.User,{as:'Users',foreignKey:'user_id',targetKey:'id'});
+    db.User.hasMany(db.Document, {foreignKey:'user_id'});
+    
+    await sequelize.sync();
+}
---
+++ _tests_/simple.test.js
@@ -0,0 +1,14 @@
+//import express from 'express';
+//import request from 'supertest';
+//const app = new express();
+
+describe('Index JS routes', () => {
+
+    test("GET /healthz", async () => {
+        expect(200).toBe(200);
+    });
+    
+    
+    });
+    
+    
